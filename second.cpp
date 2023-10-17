e2d662d7b1c2f370b8b45b81d7b504768faf3c72
Lepiev <malik.lep@mail.ru>
2023-10-17 12:46:34 +0300

    add second.cpp

---
+++ second.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+	if (argc<2){
+		std::cout << "HELLO  IDK WHO ARE YOU" << std::endl;
+
+	}else{
+		std::cout << "HI, "<< argv[1] << std::endl;
+	}
+
+
+
+
+	return 0;
+
+
+
+
+}
